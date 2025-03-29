9fdbcab7d3f6cc38dad72c1dfb4833aeb9e65792
aleksigl <112393919+aleksigl@users.noreply.github.com>
2025-03-29 22:31:20 +0100

    Add new functions: add, update, select, delete

--- db_functions.py
+++ db_functions.py
@@ -19,13 +19,6 @@ def execute_sql(conn, sql):
     except Error as error:
         print(error)
 
-def add_column(conn, table, column_name, column_type):
-    sql = f"ALTER TABLE {table} ADD COLUMN {column_name} {column_type}"
-    cur = conn.cursor()
-    cur.execute(sql)
-    conn.commit()
-    print(f"'{column_name}' column was added to '{table}' table.")
-
 
 create_topics_sql = """
 CREATE TABLE IF NOT EXISTS topics (
@@ -53,7 +46,107 @@ HowtoID integer PRIMARY KEY,
 Name varchar(255) NOT NULL,
 Tools varchar(255),
 SME varchar(255),
-Steps varchar(255),
-FOREIGN KEY (Steps) REFERENCES steps(Name)
+Step integer,
+FOREIGN KEY (Step) REFERENCES steps(StepID)
 );
 """
+
+
+def add_column(conn, table, column_name, column_type):
+    cur = conn.cursor()
+    cur.execute(f"PRAGMA table_info({table})")
+    columns = cur.fetchall()
+
+    if any(column[1] == column_name for column in columns):
+        print(f"Column '{column_name}' already exists in table '{table}'. Skipping addition.")
+        return
+
+    sql = f"ALTER TABLE {table} ADD COLUMN {column_name} {column_type}"
+    try:
+        cur.execute(sql)
+        conn.commit()
+        print(f"'{column_name}' column was added to '{table}' table.")
+    except Error as error:
+        print(error)
+
+
+def add_topic(conn, topic):
+    sql = """INSERT INTO topics(TopicID, Area, PersonResponsible1, PersonResponsible2, Name)
+                VALUES (?, ?, ?, ?, ?)"""
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, topic)
+        conn.commit()
+        return cur.lastrowid
+    except Error as error:
+        print(error)
+
+
+def add_step(conn, step):
+    sql = """INSERT INTO steps(StepID, Name, Description, TopicID, Deadline)
+                VALUES (?, ?, ?, ?, ?)"""
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, step)
+        conn.commit()
+        return cur.lastrowid
+    except Error as error:
+        print(error)
+
+
+def add_howto(conn, howto):
+    sql = """INSERT INTO howtos(HowtoID, Name, Tools, SME, Step)
+                VALUES (?, ?, ?, ?, ?)"""
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, howto)
+        conn.commit()
+        return cur.lastrowid
+    except Error as error:
+        print(error)
+
+
+def select_topic_by_area(conn, area):
+    cur = conn.cursor()
+    try:
+        cur.execute("SELECT * FROM tasks WHERE Area=?", (area,))
+        rows = cur.fetchall()
+        return rows
+    except Error as error:
+        print(error)
+
+
+def select_all(conn, table):
+    try:
+        cur = conn.cursor()
+        cur.execute(f"SELECT * FROM {table}")
+        rows = cur.fetchall()
+        return rows
+    except Error as error:
+        print(error)
+
+
+def update(conn, table, id, **kwargs):
+    parameters = [f"{k} = ?" for k in kwargs]
+    parameters = ", ".join(parameters)
+    values = tuple(v for v in kwargs.values())
+    values += (id,)
+
+    sql = f""" UPDATE {table}
+             SET {parameters}
+             WHERE Name = ?"""
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, values)
+        conn.commit()
+        print("OK")
+    except Error as error:
+        print(error)
+
+
+def delete_all(conn, table):
+    sql = f"DELETE FROM {table}"
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+    print(f"{table} deleted.")
--- main.py
+++ main.py
@@ -4,10 +4,39 @@ from db_functions import *
 if __name__ == '__main__':
     db_file = "campervan_database.db"
     conn = create_connection("campervan_database.db")
+    conn.execute('PRAGMA foreign_keys = ON;')
+
     if conn is not None:
         execute_sql(conn, create_topics_sql)
         execute_sql(conn, create_steps_sql)
         execute_sql(conn, create_howtos_sql)
-        add_column(conn, "topics", "Name", "varchar(255)")
-        conn.close()
 
+        add_column(conn, "topics", "Name", "varchar(255)")
+        topic = (1, "Water system", "Ola", "-", "Outdoor shower")
+        topic_id = add_topic(conn, topic)
+        step = (1,
+                "Rearrange pipes",
+                "Increase the length of the outdoor shower pipe",
+                topic_id,
+                "2025-06-20 00:00:00")
+        step_id = add_step(conn, step)
+        howto = (1,
+                 "Dividing the pipe",
+                 "Pipe splitter",
+                 "Obi Van Kenobi",
+                 step_id)
+        howto_id = add_howto(conn, howto)
+        print(f"Inserted Topic ID: {topic_id}, Step ID: {step_id}, Howto ID: {howto_id}")
+
+        select_topic_by_area(conn, "Water system")
+        select_all(conn, "howtos")
+
+        update(
+            conn,
+            "steps",
+            "Rearrange pipes",
+            Description="Increase the length and the diameter of the outdoor shower pipe"
+        )
+
+        delete_all(conn, "howtos")
+        conn.close()
